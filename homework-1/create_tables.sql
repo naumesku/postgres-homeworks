5bbf265f8c497af6f84e395f5b12b0f20b82d8aa
Dmitriy Naymov <prp4@bk.ru>
2023-10-07 15:47:58 +0300

    homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE employees
+(
+    employee_id serial PRIMARY KEY,
+    first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title varchar(50) NOT NULL,
+	birth_date date NOT NULL,
+    notes text NOT NULL
+);
+
+CREATE TABLE customers
+(
+    customer_id int PRIMARY KEY,
+    company_name char(5) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id serial PRIMARY KEY,
+	customer_id int UNIQUE REFERENCES customers(customer_id) NOT NULL,
+    employee_id int UNIQUE REFERENCES employees(employee_id) NOT NULL,
+	order_date date NOT NULL,
+	ship_city varchar(50) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,27 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+from pathlib import Path
+import csv
+
+PATH_ORDERS = Path(__file__).parent.joinpath("north_data").joinpath("orders_data.csv")
+PATH_EMPLOYEES = Path(__file__).parent.joinpath("north_data").joinpath("employees_data.csv")
+PATH_CUSTOMERS_DATA = Path(__file__).parent.joinpath("north_data").joinpath("customers_data.csv")
+#Пути к файлам
+
+def filling_table(path, table):
+    '''Наполняет таблицу данными из csv файла'''
+    with psycopg2.connect(host="localhost", database="north", user="postgres", password="qqqzzz") as conn:
+        with conn.cursor() as cur:
+            with open(path, 'r') as csv_file:
+                csv_data = csv.reader(csv_file)
+                next(csv_data)
+                for line in csv_data:
+                    values = ("%s," * len(line))[:-1]
+                    cur.execute(f"INSERT INTO {table} VALUES ({values})", line)
+                    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    filling_table(PATH_CUSTOMERS_DATA, "customers")
+    filling_table(PATH_EMPLOYEES, "employees")
+    filling_table(PATH_ORDERS, "orders")
